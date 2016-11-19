6d9516de149c8b3f1c2f41fdcdbd08e8d44baa24
Doug Bell <doug@preaction.me>
2016-11-19 11:45:49 -0600

    add release query methods
    This new module allows for querying the release data based on dist,
    author, and date. This is going to be used to build the API that
    metacpan needs to get at the release data.

---
+++ lib/CPAN/Testers/Schema/ResultSet/Release.pm
@@ -0,0 +1,79 @@
+use utf8;
+package CPAN::Testers::Schema::ResultSet::Release;
+our $VERSION = '0.002';
+# ABSTRACT: Query the per-release summary testers data
+
+=head1 SYNOPSIS
+
+    my $rs = $schema->resultset( 'Release' );
+    $rs->by_dist( 'My-Dist' );
+    $rs->by_author( 'PREACTION' );
+    $rs->since( '2016-01-01T00:00:00' );
+
+=head1 DESCRIPTION
+
+This object helps to query the per-release test report summaries. These
+summaries say how many pass, fail, NA, and unknown results a single
+version of a distribution has.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<DBIx::Class::ResultSet>
+
+=item L<CPAN::Testers::Schema>
+
+=item L<http://github.com/cpan-testers/cpantesters-project>
+
+For an overview of how the CPANTesters project works, and for information about
+project goals and to get involved.
+
+=cut
+
+use CPAN::Testers::Schema::Base;
+use base 'DBIx::Class::ResultSet';
+
+=method by_dist
+
+    $rs = $rs->by_dist( 'My-Dist' );
+
+Add a dist constraint to the query, replacing any previous dist
+constraints.
+
+=cut
+
+sub by_dist( $self, $dist ) {
+    return $self->search( { 'me.dist' => $dist } );
+}
+
+=method by_author
+
+    $rs = $rs->by_author( 'PREACTION' );
+
+Add an author constraint to the query, replacing any previous author
+constraints.
+
+=cut
+
+sub by_author( $self, $author ) {
+    return $self->search( { 'upload.author' => $author }, { join => 'upload' } );
+}
+
+=method since
+
+    $rs = $rs->since( '2016-01-01T00:00:00' );
+
+Restrict results to only those that have been updated since the given
+ISO8601 date.
+
+=cut
+
+sub since( $self, $date ) {
+    my $fulldate = $date =~ s/[-:T]//gr;
+    $fulldate = substr $fulldate, 0, 12; # 12 digits makes YYYYMMDDHHNN
+    return $self->search( { 'report.fulldate' => { '>=', $fulldate } }, { join => 'report' } );
+}
+
+1;
+__END__
